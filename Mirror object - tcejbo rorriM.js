229ee808621fd9e3258af5a27c9174106642e906
Dritan <105552400+DritanR@users.noreply.github.com>
2023-06-24 11:48:37 +0200

    Add files via upload

---
+++ Mirror object - tcejbo rorriM.js
@@ -0,0 +1,24 @@
+/* Mirror - Mirror
+Can you mirror the properties on an object?
+
+Given an object with properties with no value
+
+abc: -
+arara: -
+xyz: -
+Return a new object that have the properties with its mirrored key!
+
+abc: cba
+arara: arara
+xyz: zyx
+"You cannot change the original object, because if you did that the reflection would change."
+
+ */
+
+const mirror = obj => {
+  let nObj = {}
+  for (let key in obj) {
+    nObj[key] = key.split('').reverse().join('')
+  }
+  return nObj
+};
