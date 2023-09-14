afd69d7a303269d815b94c412cf082c4a71635c4
Dritan <105552400+DritanR@users.noreply.github.com>
2023-09-14 12:56:34 +0200

    Add files via upload

---
+++ Typist.js
@@ -0,0 +1,55 @@
+/* Task
+John is a typist. He has a habit of typing: he never use the Shift key to switch case, just using only Caps Lock.
+
+Given a string s. Your task is to count how many times the keyboard has been tapped by John.
+
+You can assume that, at the beginning the Caps Lock light is not lit.
+
+Input/Output
+[input] string s
+
+A non-empty string. It contains only English letters(uppercase or lowercase).
+
+1 ≤ s.length ≤ 10000
+
+[output] an integer
+
+The number of times John tapped the keyboard.
+
+Example
+For s = "a", the output should be 1.
+
+John hit button a.
+
+For s = "aa", the output should be 2.
+
+John hit button a, a.
+
+For s = "A", the output should be 2.
+
+John hit button Caps Lock, A.
+
+For s = "Aa", the output should be 4.
+
+John hit button Caps Lock, A, Caps Lock, a. */
+
+function typist(s) {
+  
+  const lAl = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+  const uAl = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+  
+    let touched = 0
+    
+    for (let i = 0; i < s.length; i++) {
+      if (s[i - 1] === undefined && uAl.includes(s[i])) {
+        touched += 2
+      } else if (uAl.includes(s[i - 1]) && lAl.includes(s[i])) {
+        touched += 2
+      } else if (lAl.includes(s[i - 1]) && uAl.includes(s[i])) {
+        touched += 2
+      } else {
+        touched += 1
+      }
+    }
+    return touched
+  }
